230f79a08f8855444319c1db551693670e9d37df
Venkat-katta <prasadkatta4415@gmail.com>
2025-01-27 14:48:00 +0530

    changes added

--- forloop.js
+++ forloop.js
@@ -1,21 +1,20 @@
-let student =[
-{id:1,s_name:'venkat',city:'hyderabad'},
-{id:2,s_name:'charan',city:'bangalore'},
-{id:3,s_name:'manoj',city:'rajamundry'},
-{id:4,s_name:'kartikeya',city:'vijyawada'},
-{id:5,s_name:'vijay',city:'chennai'},
-{id:6,s_name:'kabir',city:'kadapa'},
+let student = [
+    {id:1,s_name:'venkat',city:'hyderabad'},
+    {id:2,s_name:'charan',city:'bangalore'},
+    {id:3,s_name:'manoj',city:'rajamundry'},
+    {id:4,s_name:'kartikeya',city:'vijyawada'},
+    {id:5,s_name:'vijay',city:'chennai'},
+    {id:1,s_name:'kabir',city:'kadapa'}
+
 
 ]
 
-console.log(`output for using for in-loop`)
-for(let i in students){
+console.log(`OUTPUT FOR USING FOR IN LOOP`)
+for(let i in student ){
+    console.log(`${student[i].s_name} from ${student[i].city}`)
+}
 
-      console.log(`${students[i].s_name} from ${students[i].city}`)
-    }
-
-console.log(`output for using for of-loop`)
-for(let i of students){
-    
-    console.log(`${i.s_name} form ${i.city}`)
+console.log(`OUTPUT FOR USING FOR OF LOOP`)
+for(let i of student){
+    console.log(`${i.s_name} from ${i.city}`)
 }
